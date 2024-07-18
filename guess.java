69cdf5f3feb0b693873a4da91989605d7f61293a
Panga tulasee ram <99887635+Tulaseeram-panga@users.noreply.github.com>
2024-07-18 09:36:14 +0530

    Update guess.java

--- guess.java
+++ guess.java
@@ -1,10 +1,5 @@
 import java.util.Random;
 import java.util.*;
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
- */
-
 /**
  *
  * @author tulasee ram panga
